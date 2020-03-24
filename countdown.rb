216f2e0e9e20c3260af4a483795dcfc11cbc8861
Timothy Harling <timoharling@gmail.com>
2020-03-24 22:58:17 +0000

    Automatically backed up by Learn

---
+++ countdown.rb
@@ -0,0 +1,23 @@
+def countdown(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(num)
+
+while num > 0
+
+  puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+end
+  "HAPPY NEW YEAR!"
+end	end
