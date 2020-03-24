b120c10bc982516d9795029df12c61f05b8f93f5
Timothy Harling <timoharling@gmail.com>
2020-03-24 22:51:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,8 @@
 def countdown
   x =10 
-  while x > 0 
-  puts "#{x} SECONDS!"
-  x -= 0 
-  puts "Happy New Year!"
+    while x > 0 
+    puts "#{x} SECONDS!"
+    x -= 0 
+    puts "Happy New Year!"
+  end 
 end
